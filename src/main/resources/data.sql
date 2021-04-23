945b29360deb47b958df1d359ad3c389b939904a
hodeiez <hodeinaiz@hotmail.com>
2021-04-23 08:58:58 +0200

    testing to data.sql for right deployment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-REPLACE INTO roles VALUES (1,'MEMBER')
+-- INSERT INTO roles VALUES (1,'MEMBER')
  -- to deploy in heroku change replace for insert (postgress doesnt understand replace)
+UPDATE roles SET role='MEMBER' WHERE role_id = 1;
