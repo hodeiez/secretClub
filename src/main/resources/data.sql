46bf20b23943eede15df62068f1037bdd8eafb9e
hodeiez <hodeinaiz@hotmail.com>
2021-04-29 12:22:48 +0200

    some styling

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
- INSERT INTO roles VALUES (1,'MEMBER')
+  INSERT INTO roles VALUES (1,'MEMBER')
  -- to deploy in heroku change replace for insert (postgress doesnt understand replace)
 -- UPDATE roles SET role='MEMBER' WHERE role_id = 1;
 -- still getting errors
